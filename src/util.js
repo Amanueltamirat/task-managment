a40abbac19a9ed683c8bd1befdd7077c3d2e25c8
https://github.com/Amanueltamirat <amanuel.tamirat22@gmail.com>
2024-04-24 15:27:25 -0700

    updated

--- src/util.js
+++ src/util.js
@@ -1,1 +1,2 @@
-export const BASE_URL = 'http://localhost:5000'
+// export const BASE_URL = 'http://localhost:5000'
+export const BASE_URL = 'https://6628f9eb59b0f11708873cf1--resilient-fudge-a0676f.netlify.app'
